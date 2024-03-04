4b3d34ca94c0967e8f7c31f1e15aeb8f5b595557
TamNhien <canhthien91@gmail.com>
2024-03-04 21:31:08 +0700

    Add project files.

---
+++ LTCB_XuatSoChanTu1DenN.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Nhap so nguyen n: ";
+    cin >> n;
+
+    // In các số chẵn từ 1 đến n
+    cout << "Cac so chan tu 1 den " << n << " la : ";
+    for (int i = 2; i <= n; i += 2) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
